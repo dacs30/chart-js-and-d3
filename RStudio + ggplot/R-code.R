7a036208067d9f878a64f2dcfb606e237eac16ab
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2022-01-25 20:14:01 -0500

    R plot done + screenshot

--- RStudio + ggplot/R-code.R
+++ RStudio + ggplot/R-code.R
@@ -3,5 +3,5 @@ library(ggplot2)
 carsSample <- read.csv(file = './cars-sample.csv')
 
 
-ggplot(data = carsSample, aes(x = MPG, y = Horsepower)) +
-  geom_point()
+ggplot(data = carsSample, aes(x = Weight, y = MPG, color = Manufacturer, size = Weight)) +
+  geom_point(alpha = 0.5)
